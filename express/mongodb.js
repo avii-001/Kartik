40ea6d63797a48540958cd34efb3ecb3b0554347
avii-001 <88193025+avii-001@users.noreply.github.com>
2023-01-10 08:58:11 +0545

    Update mongodb.js

--- express/mongodb.js
+++ express/mongodb.js
@@ -1,3 +1,58 @@
+show dbs  // view the existing databases
+use <db_name> // use selected database
+db // view the current database in use
+show collections // view the list of collections
+create collection  // db.createCollection(“<colection_name>”)
+db.dropDatabase()
+db.<collection_name>.<method>
+
+Create 
+insertOne  // {}
+insertMany   // [{},{}]
+Read
+find()
+find({filterObject})
+find({filtctOberObject},{seleject})
+findOne({filterObject})
+
+Update
+updateOne({filterObject},{})
+updateMany({filterObject}.{})
+replaceOne({filterObject},{})
+Delete
+deleteOne({filterObject})
+deleteMany({filterObject})
+
+
+Filter Operators  // query operators
+$eq
+$ne
+$gt / $gte
+$lt / $lte
+$in
+$nin
+
+$not
+$and
+$or
+$exists
+
+Update Operators
+$set
+$inc
+$rename
+$unset
+
+Read Modifiers
+
+sort
+limit
+skip
+
+
+
+
+
 
 /* CRUD */
 db.users.insertOne({
@@ -59,3 +114,7 @@ db.products.updateMany({},{$set:{price:0}})
 db.products.find({brands:"puma",price:{$lt:1000},price:{$gte:500}})
 
 db.products.find({$or:[{price:{$lt:1000}},{brands:"puma"}]})
+
+
+
+
